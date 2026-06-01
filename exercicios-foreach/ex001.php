83544ea3b849f84a982e9396774bb1537a80d3e0
apollorx94 <lucas.apolinario.github@gmail.com>
2026-06-01 15:01:18 -0300

    Ex001 concluido

--- exercicios-foreach/ex001.php
+++ exercicios-foreach/ex001.php
@@ -1,2 +1,11 @@
 <?php
 // Ex 1 — Lista de tarefas: Cria um array com 5 tarefas do dia. Percorre e imprime numerado:
+
+$listaTarefas = ['Acordar', 'Levantar', 'Tomar Banho', 'Escovar os Dentes', 'Por a roupa'];
+$contador = 1;
+
+foreach ($listaTarefas as $lista) {
+
+    echo $contador . "º:" . $lista . "\n";
+    $contador++;
+}
